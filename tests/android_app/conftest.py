12398dffb1285a97d196de71d3e8812dfd58e487
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-08-15 14:54:38 +0300

    android tests

--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -5,33 +5,43 @@ from appium.options.android import UiAutomator2Options
 from selene import browser, support
 import os
 from dotenv import load_dotenv
-from selene_in_action import utils
 from appium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def load_env():
     load_dotenv()
 
+def attach_bstack_video(session_id):
+    import requests
+    bstack_session = requests.get(
+        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
+        auth=(os.getenv("BS_USERNAME"), os.getenv("BS_KEY")),
+    ).json()
+    video_url = bstack_session['automation_session']['video_url']
+
+    allure.attach(
+        '<html><body>'
+        '<video width="100%" height="100%" controls autoplay>'
+        f'<source src="{video_url}" type="video/mp4">'
+        '</video>'
+        '</body></html>',
+        name='Видео теста',
+        attachment_type=allure.attachment_type.HTML,
+    )
+
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
     options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        # 'platformName': 'android',
-        'platformVersion': '9.0',
-        'deviceName': 'Google Pixel 3',
-
-        # Set URL of the application under test
-        'app': 'bs://sample.app',
-
-        # Set other BrowserStack capabilities
+        "platformName": "android",
+        "platformVersion": os.getenv("ANDROID_PLATFORM_VERSION"),
+        "deviceName": os.getenv("ANDROID_DEVICE_NAME"),
+        "app": os.getenv("APP_ANDROID"),
         'bstack:options': {
-            'projectName': 'First Python project',
-            'buildName': 'browserstack-build-1',
-            'sessionName': 'BStack first_test',
-
-            # Set your access credentials
-            'userName': "serg_6TkIZu",
-            'accessKey': "qv4bexc8DqzCH989uTJg",
+            "projectName": "Browserstack Android Tests",
+            "buildName": "Browserstack-build-1",
+            "sessionName": "Search Test",
+            "userName": os.getenv("BS_USERNAME"),
+            "accessKey": os.getenv("BS_KEY")
         }
     })
 
@@ -54,21 +64,18 @@ def mobile_management():
 
     allure.attach(
         browser.driver.get_screenshot_as_png(),
-        name='screenshot',
+        name='Screenshot',
         attachment_type=allure.attachment_type.PNG,
     )
 
     allure.attach(
         browser.driver.page_source,
-        name='screen xml dump',
+        name='XML screen structure',
         attachment_type=allure.attachment_type.XML,
     )
 
     session_id = browser.driver.session_id
-
-    with allure.step('tear down app session'):
-        browser.quit()
-
-    utils.allure.attach_bstack_video(session_id)
+    browser.quit()
+    attach_bstack_video(session_id)
 
 
--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -13,3 +13,37 @@ def test_search():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+def test_search_wikipedia():
+
+    with step('Skip onboarding'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
+
+    with step('doubletap on search to activate searchfield'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/nav_tab_search")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/nav_tab_search")).click()
+
+    with step('Type search request'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+def test_search_wikipedia_skateboarding():
+        with step('Skip onboarding'):
+            browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
+
+        with step('doubletap on search to activate searchfield'):
+            browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/nav_tab_search")).click()
+            browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/nav_tab_search")).click()
+
+        with step('Type search request'):
+            browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Skateboarding')
+
+        with step('Verify content found'):
+            results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+            results.should(have.size_greater_than(0))
+            results.first.should(have.text('Skateboarding'))
+
